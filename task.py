f22d1c077a0853cd2ad49f6faac2ac70b0e5c1d9
obich <g.shchet@gmail.com>
2024-03-09 12:07:00 +0300

    answer

--- task.py
+++ task.py
@@ -1,55 +1,57 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def form_sample():
-    if request.method == 'GET':
-        return f'''<!doctype html>
+    return f'''<!doctype html>
                         <html lang="en">
                           <head>
                             <meta charset="utf-8">
                             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+                            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
                             <link rel="stylesheet"
                             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
                             crossorigin="anonymous">
                             <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" 
-                            /> <title>Пример формы</title> </head> <body> <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                              <ol class="carousel-indicators">
-                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                              </ol>
-                              <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                  <img class="d-block w-100" src="static/img/photo2.jpg" alt="First slide">
+                            /> <title>Пример формы</title> 
+                            </head> 
+                            <body>
+                            <center> 
+                                <h1>Пейзажи Земли</h1>
+                                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                  <ol class="carousel-indicators">
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                  </ol>
+                                  <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                      <img class="d-block w-100" src="static/img/photo1.jfif" alt="First slide" width="800" height="600">
+                                    </div>
+                                    <div class="carousel-item">
+                                      <img class="d-block w-100" src="static/img/photo2.jpg" alt="Second slide" width="800" height="600">
+                                    </div>
+                                    <div class="carousel-item">
+                                      <img class="d-block w-100" src="static/img/photo3.jpg" alt="Third slide" width="800" height="600">
+                                    </div>
+                                  </div>
+                                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                  </a>
+                                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                  </a>
                                 </div>
-                                <div class="carousel-item">
-                                  <img class="d-block w-100" src="static/img/photo1.jpg" alt="Second slide">
-                                </div>
-                                <div class="carousel-item">
-                                  <img class="d-block w-100" src="static/img/photo3.jpg" alt="Third slide">
-                                </div>
-                              </div>
-                              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                              </a>
-                              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                              </a>
-                            </div>
+                            </center>
                           </body>
                         </html>'''
-    elif request.method == 'POST':
-        f = request.files['file']
-        f = f.read()
-        with open("static/img/photo.jpg", 'wb') as file:
-            file.write(f)
-        return "Форма отправлена"
 
 
 if __name__ == '__main__':
